419bc12a81e2eb21eb6f09e535ec8d73bb5b8a33
changmin Lee <lcmini6528@gmail.com>
2018-04-18 15:38:05 +0900

    주석 추가

--- decisionTree.py
+++ decisionTree.py
@@ -4,52 +4,67 @@ from collections import Counter, defaultdict
 import itertools
 from utils import *
 
-def makeTree(D, attrs):
+def makeTree(D, attrs, pVote = None):
 
     num_inputs = len(D)
-    labelsCount = Counter([label[className] for _, label in D])
+    labelsCount = list(filter(lambda x: x[1] != 0,Counter([l[className] for _, l in D]).items()))
 
-    l = list(filter(lambda x: x[1] != 0,labelsCount.items()))
-    if l.count == 1:
-        return l[1][0]
+    #한 클래스만 존재한다면 return
+    if labelsCount.count == 1:
+        return labelsCount[1][0]
 
-    c = [(x,y) for x,y in labelsCount.items()]
-    ll = list(sorted(c, key=lambda x: x[1], reverse=True))
+    #클래스들의 빈도를 내림차순으로 저장.
+    #빈도수가 가장 높은값이 중복 존재한다면, 인자로 받은 부모 노드에서의 결과 반영
+    ll = list(sorted(labelsCount, key=lambda x: x[1], reverse=True))
+    maxList = list(filter(lambda x: x[1] == ll[0][1], ll))
+    voteResult = ll[0][0]
 
+    if len(maxList) != 1 and pVote in [x[0] for x in maxList]:
+        voteResult = pVote
+
+    #더이상 파티션할 Attribute가 없다면, 다수결로 결과를 return
     if not attrs:
-        result = ll[0][0]
-        return result
+        return voteResult
 
+    #파티션 가능한 Attribute들을 기준으로 Information Gain값 계산.
     information_gains = {}
     for attr in attrs:
         information_gains[attr] = getExpectedInformation(D,attr)
 
+    #Info(D)의 값은 모두 동일하므로, InfoA(D)값들의 최소값을 기준으로
+    #다음 파티션할 Attribute 선정.
     nextAttr = min(information_gains, key = information_gains.get)
     partitions = makePartitions(D, nextAttr)
 
+    #선정된 Attribute를 제외하고, 다음 재귀시점으로 넘김
     remainAttrs = list(filter(lambda x: x != nextAttr, attrs))
 
-    subtrees = { attr : makeTree(subset, remainAttrs) for attr, subset in partitions.items()}
-    subtrees['EOT'] = ll[0][0]
+    nextTree = { attr : makeTree(subset, remainAttrs, ll[0][0]) for attr, subset in partitions.items()}
+    # Train되지 않은 label에 대해선 다수결 결과값 return
+    nextTree['EOT'] = voteResult
 
-    return (nextAttr, subtrees)
+    return {"nextAttr":nextAttr,
+            "subTree":nextTree}
 
 def classify(tree, d):
 
+    #클래스값이 정의되었다면, 결과 return
     if tree in list(classLabels):
         return tree
 
-    attr, subDict = tree
-    subtree_key = d[attr]
+    #다음으로 구분할 Attribute를 이용하여
+    #Data의 Label을 조회, subTree 생성
+    nextAttr = d[tree["nextAttr"]]
 
-    if subtree_key not in subDict:
-        subtree_key = 'EOT'
+    # 만약 해당 Label에 대한 트리가 만들어지지 않았다면
+    # 다수결 값 return하도록 설정.
+    if nextAttr not in tree["subTree"]:
+        nextAttr = 'EOT'
 
-    subtree = subDict[subtree_key]
+    #해당 Attribute로 subTree 접근
+    subtree = tree["subTree"][nextAttr]
 
     return classify(subtree, d)
-
-
 
 train_file = open(sys.argv[1], 'r')
 test_file = open(sys.argv[2], 'r')
@@ -60,8 +75,8 @@ trainData = train_file.read().split('\n')
 allAttributes = trainData[0].split('\t')[:-1]
 className = trainData[0].split('\t')[-1]
 classLabels = set()
-
 trainInput = []
+
 for line in trainData[1:-1]:
     dic = {}
     for val, key in zip(line.split('\t'), allAttributes):
@@ -84,6 +99,7 @@ for line in testData[1:-1]:
 
 
 resultData = []
+
 for test in testInput:
     result = classify(DTree,test)
     resultData.append((test,result))
--- utils.py
+++ utils.py
@@ -4,30 +4,27 @@ import math
 
 def getExpectedInformation(D, attr):
     partitions = makePartitions(D, attr)
-    return partition_entropy(partitions.values())
+    return calcI(partitions.values())
 
 def makePartitions(D, attr):
+    #defaultdict을 활용하여 label에 따라 파티션을 생성합니다.
     partitions = defaultdict(list)
     for data in D:
         category = data[0][attr]
         partitions[category].append(data)
     return partitions
 
-def calcSumOfI(subsets):
-    total = len([data for subset in subsets for data in subset])
-    return sum(len(subset) / total * data_entropy(subset) for subset in subsets)
+def calcI(subsets):
+    #계산된 확률들을 기반하여 InfoA(D)값을 계산합니다.
+    total = len([d for s in subsets for d in s])
+    return sum(len(s) / total * calcSumOfP(s) for s in subsets)
 
-def data_entropy(labeled_data):
-    labels = [list(label.items())[0][1] for _, label in labeled_data]
+def calcSumOfP(D):
+    #Label들의 List를 활용하여 Label별 -p * log2(p)의 합을 계산합니다.
+    labels = [list(label.items())[0][1] for _, label in D]
     ps = [float(count) / float(len(labels)) for count in Counter(labels).values()]
 
     return sum(-p * math.log(p, 2) for p in ps if p != 0)
 
 def calcGiniIndex(ps):
     return 1 - reduce(lambda x, y: x**2 + y**2, ps)
-
-
-
-
-
-
